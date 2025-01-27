0f9d5de24d91cf63a9f1da350eae344d39ca4485
Rudrajyoti Roy <48586418+rudrajyotiroy@users.noreply.github.com>
2025-01-27 13:25:26 -0500

    Direct upload initial files

---
+++ hw1pass/hw1pass.cpp
@@ -0,0 +1,45 @@
+#include "llvm/Analysis/BlockFrequencyInfo.h"
+#include "llvm/Analysis/BranchProbabilityInfo.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include  <iostream>
+
+using namespace llvm;
+
+namespace {
+
+struct HW1Pass : public PassInfoMixin<HW1Pass> {
+
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+    // These variables contain the results of some analysis passes.
+    // Go through the documentation to see how they can be used.
+    llvm::BlockFrequencyAnalysis::Result &bfi = FAM.getResult<BlockFrequencyAnalysis>(F);
+    llvm::BranchProbabilityAnalysis::Result &bpi = FAM.getResult<BranchProbabilityAnalysis>(F);
+
+    // Add your code here
+
+    return PreservedAnalyses::all();
+  }
+};
+}
+
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK llvmGetPassPluginInfo() {
+  return {
+    LLVM_PLUGIN_API_VERSION, "HW1Pass", "v0.1",
+    [](PassBuilder &PB) {
+      PB.registerPipelineParsingCallback(
+        [](StringRef Name, FunctionPassManager &FPM,
+        ArrayRef<PassBuilder::PipelineElement>) {
+          if(Name == "hw1"){
+            FPM.addPass(HW1Pass());
+            return true;
+          }
+          return false;
+        }
+      );
+    }
+  };
+}
