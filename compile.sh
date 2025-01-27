1d4d76eda847de57583f01d9e1bfe4b1ef4929da
rudrajyotiroy <rudrajyotiroy@gmail.com>
2025-01-27 13:41:38 -0500

    First build

---
+++ compile.sh
@@ -0,0 +1,13 @@
+mkdir -p build && cd build
+cmake ..
+make
+echo "Compile done"
+cd ../benchmark1
+./run.sh simple
+echo "Benchmark 1 done"
+cd ../benchmark2
+./run.sh anagram
+echo "Benchmark 2 done"
+cd ../benchmark3
+./run.sh compress
+echo "Benchmark 3 done"
